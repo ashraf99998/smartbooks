cb7d61356111ad57afb65ceb45597c895693c581
Ashraf Yousefi <ashrafyousefi@Mac.lan>
2025-04-17 12:54:11 -0500

    Add index.js to fix React rendering

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "5rem" }}>
+      <h1>✅ SmartBooks App Is Working</h1>
+      <p>You're seeing this because React is rendering properly.</p>
+    </div>
+  );
+}
+
+export default App;
